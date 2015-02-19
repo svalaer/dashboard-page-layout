e3f2554b137b7fc1101e81175298cc6ab2269f4a
svalaer <sullivanvalaer@gmail.com>
2015-02-18 22:00:43 -0800

    Added controller that connects to metrics controller. Added dynamic angular tables.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,26 @@ $(document).ready(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
+
+blocjams.controller("dashboard.controller", ["$scope","Metrics","$rootScope",function($scope,Metrics,$rootScope){
+    $scope.songPlays = $rootScope.songPlays;
+    $scope.albumClicks = $rootScope.albumClicks;
+    $scope.artistClicks = $rootScope.artistClicks;
+
+    $scope.state = "song";
+    $scope.buttonClick = function(button) {
+        $scope.state = button
+
+    };
+    $scope.state = "album";
+    $scope.buttonClick = function(button) {
+        $scope.state = button
+
+    };
+    $scope.state = "artist";
+    $scope.buttonClick = function(button) {
+        $scope.state = button
+
+    }
+
+}]);
